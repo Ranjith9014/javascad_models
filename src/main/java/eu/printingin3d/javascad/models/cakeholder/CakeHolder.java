f2507f7e7eaf960dab4e0d450740e79fa24616a8
Printing in 3D <info@printingin3d.eu>
2016-07-03 22:57:21 +0200

    Upgrading to the latest version and using some of its new features

--- src/main/java/eu/printingin3d/javascad/models/cakeholder/CakeHolder.java
+++ src/main/java/eu/printingin3d/javascad/models/cakeholder/CakeHolder.java
@@ -3,10 +3,8 @@ package eu.printingin3d.javascad.models.cakeholder;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
-import eu.printingin3d.javascad.basic.Angle;
 import eu.printingin3d.javascad.basic.Radius;
 import eu.printingin3d.javascad.coords.Angles3d;
 import eu.printingin3d.javascad.coords.Coords3d;
@@ -21,7 +19,6 @@ import eu.printingin3d.javascad.models2d.Square;
 import eu.printingin3d.javascad.tranzitions.Difference;
 import eu.printingin3d.javascad.tranzitions.Union;
 import eu.printingin3d.javascad.utils.SaveScadFiles;
-import eu.printingin3d.javascad.utils.UsefulConsts;
 
 public class CakeHolder extends Extendable3dModel {
 	private static final Radius SECTION1_D = Radius.fromDiameter(80); 
@@ -32,8 +29,10 @@ public class CakeHolder extends Extendable3dModel {
 	private static final Radius OUTER_DIAMETER = Radius.fromDiameter(160);
 	
 	private static final Radius BIG_HOLE_DIAMETER = Radius.fromDiameter(30);
+	private static final Radius BIG_HOLE_OFFSET = Radius.fromDiameter(36);
 
 	private static final Radius SMALL_HOLE_DIAMETER = Radius.fromDiameter(7);
+	private static final Radius SMALL_HOLE_OFFSET = Radius.fromDiameter(7);
 	private static final double SMALL_HOLE_DIST_FROM_ORIG = 30;
 	
 	private static final int NUMBER_OF_BIG_RINGS = 12;
@@ -55,11 +54,11 @@ public class CakeHolder extends Extendable3dModel {
 		
 		Abstract3dModel bigHole = new Cylinder(THICKNESS+1, BIG_HOLE_DIAMETER);
 		Abstract3dModel smallHole =
-			trice(new Cylinder(THICKNESS+1, SMALL_HOLE_DIAMETER), SMALL_HOLE_DIAMETER.getDiameter()*0.43)
+			new Cylinder(THICKNESS+1, SMALL_HOLE_DIAMETER).moves(SMALL_HOLE_OFFSET.toCircle(3, 0, 0))
 					.move(Coords3d.yOnly(-SMALL_HOLE_DIST_FROM_ORIG));
 		
 		this.baseModel = new Difference(base,
-				trice(bigHole, BIG_HOLE_DIAMETER.getRadius()+1),
+				bigHole.moves(BIG_HOLE_OFFSET.toCircle(3, 0, 0)),
 				smallHole,
 				smallHole.rotate(new Angles3d(0, 0, +120)),
 				smallHole.rotate(new Angles3d(0, 0, -120))
@@ -67,22 +66,16 @@ public class CakeHolder extends Extendable3dModel {
 	}
 
 	private Abstract3dModel createOuterRing() {
-		List<Coords3d> moves = new ArrayList<>();
-		
-		Angle slice = Angle.A360.divide(24);
-		
-		for (int i=0;i<24;i++) {
-			moves.add(RING1_D.toCoordinate(slice.mul(i)).withZ(0));
-			moves.add(RING2_D.toCoordinate(slice.mul(i+0.5)).withZ(0));
-		}
+		List<Coords3d> moves = new ArrayList<>(RING1_D.toCircle(24, 0, 0));
+		moves.addAll(RING2_D.toCircle(24, 0.5, 0));
 		
 		Abstract3dModel ringBase = new Cylinder(THICKNESS, OUTER_DIAMETER);
 		return new Union( 
 			new Difference(
 				ringBase,
 				new Cylinder(THICKNESS+1, SECTION2_D),
-				new Ring(RING1_D.getRadius(), new Square(new Dims2d(2, 2))).align(Side.TOP_IN, ringBase).move(Coords3d.zOnly(0.1)),
-				new Ring(RING2_D.getRadius(), new Square(new Dims2d(2, 2))).align(Side.TOP_IN, ringBase).move(Coords3d.zOnly(0.1))
+				new Ring(RING1_D, new Square(new Dims2d(2, 2))).align(Side.TOP_IN, ringBase).move(Coords3d.zOnly(0.1)),
+				new Ring(RING2_D, new Square(new Dims2d(2, 2))).align(Side.TOP_IN, ringBase).move(Coords3d.zOnly(0.1))
 			),
 			new Cylinder(THICKNESS, Radius.fromRadius(1.1)).moves(moves)
 			);
@@ -91,23 +84,8 @@ public class CakeHolder extends Extendable3dModel {
 	private Abstract3dModel createMiddleRing() {
 		Radius offset = SECTION1_D.add(SECTION2_D).divide(2);
 		
-		Angle slice = Angle.A360.divide(NUMBER_OF_BIG_RINGS);
-		
-		List<Coords3d> moves = new ArrayList<>();
-		for (int i=0;i<NUMBER_OF_BIG_RINGS;i++) {
-			moves.add(offset.toCoordinate(slice.mul(i)).withZ(0));
-		}
-		
-		return new Ring((SECTION2_D.getDiameter()-SECTION1_D.getDiameter())/4, new Square(new Dims2d(THICKNESS_THIN, THICKNESS_THIN)))
-				.moves(moves);
-	}
-	
-	private static Abstract3dModel trice(Abstract3dModel orig, double dist) {
-		return orig.moves(Arrays.asList(
-					new Coords3d(+dist, -dist/2, 0),
-					new Coords3d(-dist, -dist/2, 0),
-					Coords3d.yOnly(dist*(UsefulConsts.SQRT_3-0.5))
-				));
+		return new Ring(SECTION2_D.substract(SECTION1_D).divide(2), new Square(new Dims2d(THICKNESS_THIN, THICKNESS_THIN)))
+				.moves(offset.toCircle(NUMBER_OF_BIG_RINGS, 0.0, 0));
 	}
 
 	public static void run(File root) throws IOException {
